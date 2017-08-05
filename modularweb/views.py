dab9cdf0ee79488e604db7f44339c9343573059c
Carles Salvador Soriano Pérez <Carles Salvador Soriano Prez>
2017-08-06 01:10:53 +0200

    #7 Navbar is now a template, tabs get activated by a variable.

--- modularweb/views.py
+++ modularweb/views.py
@@ -5,18 +5,29 @@ def index(request):
     return render(request, 'index.html', {})
 
 def contact(request):
-    variables = {'pageName': 'Contact'}
+    variables = {
+    				'pageName': 'Contact',
+    				'nbar': 'contact'
+    			}
     return render(request, 'contact.html', variables)
 
 def about(request):
-    variables = {'pageName': 'About us'}
+    variables = {
+    				'pageName': 'About us',
+    				'nbar': 'about'
+    			}
     return render(request, 'aboutus.html', variables)
 
-
 def gallery(request):
-    variables = {'pageName': 'Gallery'}
+    variables = {
+    				'pageName': 'Gallery',
+    				'nbar': 'gallery'
+    			}
     return render(request, 'gallery.html', variables)
 
 def content_page_01(request):
-    variables = {'pageName': 'Content'}
+    variables = {
+    				'pageName': 'Content',
+    				'nbar': 'content'
+    			}
     return render(request, 'content_page_01.html', variables)
