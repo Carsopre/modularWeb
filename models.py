3a6b05a8ccb2d5f81a8148af465754bd9859cded
Carles S. Soriano Pérez <cssp1989@gmail.com>
2018-07-17 22:22:40 +0200

    Fixed bug.
    ContactPage should not have the method get field value if this is only going to be calling to the ´Field´ class.

---
+++ models.py
@@ -0,0 +1,99 @@
+from django.db import models
+
+# Create your models here.
+class BasicSettings(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=250)
+    email = models.CharField(max_length=50)
+    def GetAppName(self):
+        pass
+
+class Page(models.Model):
+    title = models.CharField(max_length=50)
+    slug = models.SlugField(max_length=100, unique=True)
+    body = models.TextField(blank = True)
+    def __str__(self):
+        return self.title
+    def GetPage(slugPage):
+        return Page.objects.filter( slug = slugPage ).first()
+
+class ContactPage(Page):
+    content = models.CharField(max_length=50, blank = True)
+    email = models.EmailField(blank = True)
+    phone = models.CharField(max_length=50, blank = True)
+    def GetEmail(slugContact):
+        page = ContactPage.objects.filter( slug = slugContact).first()
+        if( page is not None):
+            return page.email
+        return None
+
+    
+class Fields(models.Model):
+    slug = models.SlugField(max_length=100, unique=True)
+    name = models.CharField(max_length=50)
+    value = models.CharField(max_length=250)
+    contactPage = models.ManyToManyField(
+                            ContactPage,
+                            blank = True)
+    def GetContactFieldValue(fieldSlug):
+        field = Fields.objects.filter(slug = fieldSlug).first()
+        if(field is not None):
+            return field.value
+        return ""
+    def __str__(self):
+        return self.name
+
+class Photography(models.Model):
+    title = models.CharField(max_length=50)
+    slug = models.SlugField(max_length=100, unique=True)
+    description = models.CharField(max_length=150, blank = True)
+    url = models.CharField(max_length=200)
+    def __str__(self):
+        return self.title
+    def GetPhotography(slugPhoto):
+        return Photography.objects.filter( slug = slugPhoto ).first()
+    def GetPhotographyUrl(slugPhoto):
+        photo = Photography.GetPhotography(slugPhoto)
+        if( photo is not None):
+            return photo.url
+        ""
+
+class Gallery(models.Model):
+    slug = models.SlugField(max_length=100, unique=True)
+    def __str__(self):
+        return self.slug
+    def GetGalleryPhotographies(self):
+        galleryPhotos = GalleryPhotographies.objects.filter( gallery = self )
+        photographies = []
+        for galleryPhoto in galleryPhotos:
+            photographies.append( galleryPhoto.photography )
+        return photographies
+    def GetGalleryPhoto(self, photoSlug):
+        photoGallery = GalleryPhotographies.objects.filter(gallery = self, photography__slug = photoSlug).first()
+        if(photoGallery is not None):
+            return photoGallery.photography
+        return None
+    
+class GalleryPhotographies(models.Model):
+    gallery = models.ForeignKey(
+                        Gallery,
+                        on_delete=models.CASCADE)
+    photography = models.ForeignKey(
+                        Photography,
+                        on_delete=models.CASCADE)
+    def __str__(self):
+        return self.gallery.slug + ", " + self.photography.title
+    class Meta:
+        ordering = ('gallery',)
+
+class GalleryPage(Page):
+    gallery = models.ForeignKey(
+                        Gallery,
+                        on_delete=models.CASCADE)
+
+    def getGalleryPhotographies(self):
+        galleryPhotos = GalleryPhotographies.objects.filter( gallery = self.gallery )
+        photographies = []
+        for galleryPhoto in galleryPhotos:
+            photographies.append( galleryPhoto.photography )
+        return photographies
